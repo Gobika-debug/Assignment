0f66bdfc4dd458b2c385e2acd154158afd24afe2
Gobika M <22ec021@psr.edu.in>
2025-06-13 21:41:02 +0530

    Create MethodMissing.groovy

---
+++ Assignment-21/MethodMissing.groovy
@@ -0,0 +1,8 @@
+class DynamicMethodHandler {
+    def methodMissing(String name, args) {
+        println "Method $name is not defined"
+    }
+}
+
+def obj = new DynamicMethodHandler()
+obj.unknownMethod()
